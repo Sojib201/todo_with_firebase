8895d792337a82e48e2b6319842b526abdb46269
Md Sojib Molla <sojib.cse3729@gmail.com>
2025-04-21 17:16:42 +0600

    first

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,40 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 50, left: 30),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.green, Colors.teal])),
+        child: Column(
+          //: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hello \n Sojib',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Card(
+                  elevation: 5,
+                  child: Container(
+                    child: Text('Today'),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
